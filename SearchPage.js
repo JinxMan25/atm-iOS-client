f9ae32d58e9240f1c9295857d681361259f198a7
MD Huq <samiulg3@gmail.com>
2015-04-03 20:06:51 -0400

    complete view

--- SearchPage.js
+++ SearchPage.js
@@ -24,9 +24,45 @@ var styles = StyleSheet.create({
     padding: 30,
     marginTop: 65,
     alignItems: 'center'
+  },
+  flowRight: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    alignSelf: 'stretch'
+  },
+  buttonText: {
+    fontSize: 18,
+    color: 'white',
+    alignSelf: 'center'
+  },
+  button: {
+    height: 36,
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: '#48BBEC',
+    borderColor: '#48BBEC',
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 10,
+    alignSelf: 'stretch',
+    justifyContent: 'center'
+  },
+  searchInput: {
+    height: 36,
+    padding: 4,
+    marginRight: 5,
+    flex: 4,
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: '#48BBEC',
+    borderRadius: 8,
+    color: '#48BBEC'
+  },
+  image: {
+    width: 217,
+    height: 138
   }
 });
-
 class SearchPage extends Component {
   render(){
     return (
@@ -37,6 +73,18 @@ class SearchPage extends Component {
         <Text style={styles.description}>
           Search for place-name, postcode or search near your location.
         </Text>
+        <View style={styles.flowRight}>
+          <TextInput style={styles.searchInput} placeholder="Search via name or post code"/>
+          <TouchableHighlight style={styles.button}
+            underlayColor="#99d9f4">
+            <Text style={styles.buttonText}>Go</Text>
+          </TouchableHighlight>
+        </View>
+        <TouchableHighlight style={styles.button}
+          underlayColor="#99d9f4">
+          <Text style={styles.buttonText}>Location</Text>
+        </TouchableHighlight>
+        <Image source={require('image!house')} style={styles.image}/>
       </View>
         );
   }
